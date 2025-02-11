b34d2d5e0a7699b1a115ae631e43725d66779eea
Lab05_etec <Lab05_etec@etec>
2025-02-11 20:29:06 -0300

    Criação do links

--- contato.php
+++ contato.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +7,7 @@
     <title>Entre em contato</title>
 </head>
 <body>
+    <a href="index.php">Página inicial</a>
     <?php
     echo "<h1> Página de contato do site</h1>";
     ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,8 +7,9 @@
     <title>Document</title>
 </head>
 <body>
+    <a href="contato.php">Página de contato </a>
     <?php
-    echo "<h1> Titulo do prgeto PWIIB</h1>"
+    echo "<h1> Titulo do progeto PWIIB</h1>"
     
     ?>
 </body>
