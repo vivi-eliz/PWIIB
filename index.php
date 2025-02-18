12fa8c21b0bad2d13f3d903a0ae74846449db78d
Lab05_etec <Lab05_etec@etec>
2025-02-18 20:41:43 -0300

    div

--- index.php
+++ index.php
@@ -6,139 +6,45 @@
     <meta name="viewport" content="width=<, initial-scale=1.0">
     <title>Document</title>
      <link rel="stylesheet" href="bootstrap.min.css">
+
+<style>
+  
+   .row{
+     border: 2px solid red;
+     padding: 5px:
+   }
+
+    .row > div {
+      border: 2px solid yellow;
+      padding: 50px 0;
+      text-align: center
+    }
+
+</style>
+
 </head>
 <body>
-
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
+ <div class="row">
+    <div class="col-2">
+    <button type="button" class="btn btn-outline-info">Cadastros</button>
+    </div>
+    <div class="col-8">
+       pesquisar
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">goopokemon</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
-  </div>
-</nav>
+    <div class="col-2">
+    <button type="button" class="btn btn-outline-info">Procurar</button>
+    </div>
 
 
 
-    <a href="contato.php">Página de contato </a>
-    <?php
-    echo "<h1> Titulo do progeto PWIIB</h1>"
-    
+ </div>
+
+
+<script src="bootstrap.bundle.min.js" ></script>
+<script src="bootstrap.min.css" ></script>
  
-    ?>
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<br>
-
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Assinar tomato </a></li>
-    <li><a class="dropdown-item" href="#">assirtir pokemon</a></li>
-    <li><a class="dropdown-item" href="#">qual é o seu pokemon?</a></li>
-  </ul>
-</div>
-
-<br>
-
-<ol class="list-group list-group-numbered">
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">pokemon de agua </div>
-      Content for list item
-    </div>
-    <span class="badge text-bg-primary rounded-pill">14</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">pokemon fantasma </div>
-      Content for list item
-    </div>
-    <span class="badge text-bg-primary rounded-pill">14</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">pokemon raro</div>
-      Content for list item
-    </div>
-    <span class="badge text-bg-primary rounded-pill">14</span>
-  </li>
-</ol>
-
-<br>
 
 
-
-   <script src="bootstrap.bundle.min.js" ></script>
 </body>
 </html>
