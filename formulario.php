eea091192ed48d9034df72353a4046b7cf8d1c75
Lab05_etec <Lab05_etec@etec>
2025-03-21 22:41:37 -0300

    ,,

--- formulario.php
+++ formulario.php
@@ -67,6 +67,12 @@
 
 </form> -->
 <br>
+
+1- Crie um formulário HTML que solicite o 
+    nome e a idade de um usuário.
+ Ao enviar o formulário com o método GET, 
+ exiba o nome e a idade do usuário na página seguinte.
+
 <form  action="salvar_dados.php" method="get">
     <label>Nome</label>
 <input type="text" name="nome" value="">
@@ -79,32 +85,72 @@
 </button>
 
 </form>
-
-<!-- 1- Crie um formulário HTML que solicite o 
-    nome e a idade de um usuário.
- Ao enviar o formulário com o método GET, 
- exiba o nome e a idade do usuário na página seguinte.
+<br>
 
 2-Crie um formulário HTML que receba dois números e, 
 ao enviar com o método GET, exiba a soma desses dois números.
 
+<form action="salvar_dados.php" method="get">
+
+<label>N1</label>
+<input for="number" name="n1">
+<br>
+<label>N2</label>
+<input  for="number" name="n2">
+<br>
+<button type="submit">
+  =
+</button>
+
+</form>
+<br>
 3-Crie um formulário HTML que peça o nome do usuário. 
 Quando o formulário for enviado com o método GET,
  exiba uma mensagem personalizada dizendo "Olá, [nome]".
+
+  <form  action="salvar_dados.php" method="get">
+   
+  <label>Nome</label>
+   <input type="text" name= "nome">
+   <br>
+   <button type="submit">logar</button>
+    
+   </form>
+ <br>
 
 4-Crie um formulário de login que receba o nome de usuário e senha.
  Quando o formulário for enviado com o método POST,
   verifique se o nome de usuário e a senha são válidos
    (pode ser um simples exemplo de validação).
+    
+    <form action="">
+      
+      <label>Nome</label>
+      <input type="text" name="nome">
+      <br>
+      <label>Senha</label>
+      <input type="number" name="senha">
+      <br>
+      <button type="submit">
+        logar
+      </button>
 
+    </form>
+   <br>
 5-Crie um formulário HTML que receba dois números e,
  ao enviar com o método POST, exiba o produto desses números.
+  <form action="">
 
+  </form>
+ <br>
 6-Crie um formulário HTML que receba
  o nome e a mensagem de um usuário.
  Quando o formulário for enviado com o método POST,
-  exiba a mensagem do usuário. -->
+  exiba a mensagem do usuário. 
+     <form action="">
 
+     </form>
+  <br>
 <script src="bootstrap.bundle.min.js" ></script>
 </body>
 </html>
--- salvar_dados.php
+++ salvar_dados.php
@@ -8,9 +8,31 @@ echo "<br>";
 echo $_POST["senha"];
 ?> -->
 
-<?php
+<!-- <?php
 echo $_GET["nome"];
 echo "<br>";
 echo $_GET["idade"];
+
+?> -->
+<br>
+<!-- <?php
+
+$num1= $_GET["n1"];
+echo "<br>";
+$num2= $_GET["n2"];
+$soma = $num1+$num2;
+echo "A soma de $num1 e $num2 sera igua a $soma"
+
+?> -->
+<br>
+<!-- <?php
+$f = $_GET["nome"];
+echo "Olá,$f"
+
+?> -->
+
+<?php
+
+
 
 ?>
